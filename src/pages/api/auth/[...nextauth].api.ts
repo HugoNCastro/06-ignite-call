3bd7e62931ed697f8e90b8a35bb5499bc0122082
HugoNCastro <hugonaaman@gmail.com>
2023-06-09 16:19:21 -0300

    Cria adapter para finalizar processo de criação de usuário

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,29 +1,49 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { NextAuthOptions } from 'next-auth'
 import NextAuth from 'next-auth/next'
-import GoogleProvider from 'next-auth/providers/google'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import { PrismaAdapter } from '../../../lib/auth/prisma-adapter'
 
 const googleUrlAuth = 'https://www.googleapis.com/auth'
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope: `${googleUrlAuth}/userinfo.email ${googleUrlAuth}/userinfo.profile ${googleUrlAuth}/calendar`,
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope: `${googleUrlAuth}/userinfo.email ${googleUrlAuth}/userinfo.profile ${googleUrlAuth}/calendar`,
+          },
         },
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '',
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+    ],
+    callbacks: {
+      async signIn({ account }) {
+        if (!account?.scope?.includes(`${googleUrlAuth}/calendar`)) {
+          return '/register/connect-calendar/?error=permissions'
+        }
+        return true
       },
-    }),
-  ],
-  callbacks: {
-    async signIn({ account }) {
-      if (!account?.scope?.includes(`${googleUrlAuth}/calendar`)) {
-        return '/register/connect-calendar/?error=permissions'
-      }
-      return true
     },
-  },
+  }
 }
 
-export default NextAuth(authOptions)
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
