85bcf74d326cad65c1160fd9c0d4b15eeac51b55
HugoNCastro <hugonaaman@gmail.com>
2023-06-14 17:43:51 -0300

    Finaliza persistência de perfil do usuário na aplicação

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -22,6 +22,7 @@ import {
   IntervalItem,
 } from './styles'
 import { api } from '../../../lib/axios'
+import { useRouter } from 'next/router'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -85,6 +86,8 @@ export default function TimeIntervals() {
     },
   })
 
+  const router = useRouter()
+
   const weekDays = getWeekDays()
 
   const { fields } = useFieldArray({
@@ -99,6 +102,7 @@ export default function TimeIntervals() {
     await api.post('users/time-intervals', {
       intervals,
     })
+    await router.push('/register/update-profile')
   }
 
   return (
