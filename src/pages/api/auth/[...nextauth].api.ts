26fecbc3a023babef906ab0bfa41f78a201f787c
HugoNCastro <hugonaaman@gmail.com>
2023-06-21 16:20:15 -0300

    Finaliza etapa de agendamento com vinculação ao calendário da Google

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -19,6 +19,9 @@ export function buildNextAuthOptions(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
         authorization: {
           params: {
+            prompt: 'consent',
+            access_type: 'offline',
+            response_type: 'code',
             scope: `${googleUrlAuth}/userinfo.email ${googleUrlAuth}/userinfo.profile ${googleUrlAuth}/calendar`,
           },
         },
